7b3780ec629634459272371a533d7ce91416af50
SydNS <40373744+SydNS@users.noreply.github.com>
2021-04-04 17:57:09 -0700

    seeting up the splashscreen for the mockapp

--- app/src/main/java/com/example/mobmechan/MainActivity.kt
+++ app/src/main/java/com/example/mobmechan/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.mobmechan
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(1000)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    val mainIntent = Intent(this@SplashActivity, WelcomeActivity::class.java)
+                    startActivity(mainIntent)
+                }
+            }
+        }
+        thread.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
     }
 }
