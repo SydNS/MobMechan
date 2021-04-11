2e1c4f9be7d9457c3a19d4d94f5cb6a8e2223030
SydNS <40373744+SydNS@users.noreply.github.com>
2021-04-11 03:52:49 -0700

    created an activity to be the launching activity for the app

--- app/src/main/java/com/example/mobmechan/MainActivity2.kt
+++ app/src/main/java/com/example/mobmechan/MainActivity2.kt
@@ -1,4 +1,30 @@
 package com.example.mobmechan
 
-class MainActivity2 {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(1000)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    val mainIntent = Intent(this@MainActivity2, WelcomeActivity::class.java)
+                    startActivity(mainIntent)
+                }
+            }
+        }
+        thread.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
 }
