15c42a2b9d3e4de07e3d405c5147eb57a982c208
SydNS <40373744+SydNS@users.noreply.github.com>
2021-04-13 06:14:10 -0700

    working the entry point into the app

---
+++ app/src/main/java/com/example/mobmechan/MainActivity.kt
@@ -0,0 +1,27 @@
+@file:Suppress("DEPRECATION")
+
+package com.example.mobimech.UI
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import com.example.mobmechan.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val mainactivityview: View = activityMainBinding.root
+        setContentView(mainactivityview)
+
+
+    }
+
+}
